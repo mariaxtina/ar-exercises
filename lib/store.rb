ffb99ceeb41fbb756539cfd00ebae3831eb3d164
mariaxtina <mchristinahess@gmail.com>
2016-07-26 20:36:44 -0700

    Add validation features for the employee and store

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+
+  def validate!
+    errors.add(:name, :blank, message: "cannot be nil") if name.nil?
+  end
 
 end
