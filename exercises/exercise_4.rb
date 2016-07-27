86a87328869d05bf15ed1d14d253920d137d4215
mariaxtina <mchristinahess@gmail.com>
2016-07-26 20:33:04 -0700

    Calculate revenue

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey_store = Store.create(annual_revenue: 224000, mens_apparel: false, womens_apparel: true )
+whistler_store = Store.create(annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown_store = Store.create(annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+@womens_stores = Store.where("annual_revenue < 1000000", womens_apparel: true)
+
+@womens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
